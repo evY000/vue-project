ed2dabe40f2672d323314f2918ada6fc2385c7f4
evY000 <000evan001@gmail.com>
2024-02-14 22:31:24 -0500

    asfddfgt

---
+++ vue/src/components/cards.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="Card"> <h1 class="text">{{ Name }}<img class="img" :src="Image"> <button class="btn" type="button">Select</button>  </h1></div>
+</template>
+<script setup>
+const props = defineProps({
+   Name: String,
+    Image : String
+})
+</script>
+<style lang="scss" scoped>
+</style>
