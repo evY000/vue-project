9c22eca3eb3ae3291397a5b36ca1806da744441f
evY000 <000evan001@gmail.com>
2024-03-03 21:41:53 -0500

    asdf

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,33 +1,30 @@
 <script setup>
-import Cards from '@/components/cards.vue'
-</script>
-<template>
-<div class="Card">
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Giannis Antetokounmpo" Image="https://cdn.nba.com/headshots/nba/latest/260x190/203507.png"> </Cards>
-<Cards Name="Luka Doncic" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1629029.png"> </Cards>
-<Cards Name="Joel Embiid" Image="https://cdn.nba.com/headshots/nba/latest/260x190/203954.png"> </Cards>
-<Cards Name="Kevin Durant" Image="https://cdn.nba.com/headshots/nba/latest/260x190/201142.png"> </Cards>
-<Cards Name="Tyrese Haliburton" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1630169.png"> </Cards>
-<Cards Name="Shai Gilgeous-Alexander" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1628983.png"> </Cards>
-<Cards Name="Damian Lilliard" Image="https://cdn.nba.com/headshots/nba/latest/260x190/203081.png"> </Cards>
-<Cards Name="Jayson Tatum" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1628369.png"> </Cards>
-<Cards Name="Nikola Jokic" Image="https://cdn.nba.com/headshots/nba/latest/260x190/203999.png"> </Cards>
-<Cards Name="Bam Adebayo" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1628389.png"> </Cards>
-<Cards Name="Devin Booker" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1626164.png"> </Cards>
-<Cards Name="Paolo Banchero" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1631094.png"> </Cards>
-<Cards Name="Stephen Curry" Image="https://cdn.nba.com/headshots/nba/latest/260x190/201939.png"> </Cards>
-<Cards Name="Scottie Barnes" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1630567.png"> </Cards>
-<Cards Name="Anthony Davis" Image="https://cdn.nba.com/headshots/nba/latest/260x190/203076.png"> </Cards>
-<Cards Name="Jaylen Brown" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1627759.png"> </Cards>
-<Cards Name="Anthony Edwards" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1630162.png"> </Cards>
-<Cards Name="Jalen Brunson" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1628973.png"> </Cards>
-<Cards Name="Paul George" Image="https://cdn.nba.com/headshots/nba/latest/260x190/202331.png"> </Cards>
-<Cards Name="Tyrese Maxey" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1630178.png"> </Cards>
-<Cards Name="Kawhi Leonard" Image="https://cdn.nba.com/headshots/nba/latest/260x190/202695.png"> </Cards>
-<Cards Name="Donovan Mitchell" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1628378.png"> </Cards>
-<Cards Name="Karl Anthony Towns" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1626157.png"> </Cards>
-<Cards Name="Julius Randle" Image="https://cdn.nba.com/headshots/nba/latest/260x190/203944.png"> </Cards>
-<Cards Name="Trae Young" Image="https://cdn.nba.com/headshots/nba/latest/260x190/1629027.png"> </Cards>
-</div>
+ import Info from '@/components/cards.vue' //import Cards 
+ const props = defineProps({
+   Info : Object
+ })
+ </script>
+<template> 
+ <!-- <div class="Card"> <h1 class="text">{{ Name }}<img class="img" :src="IMG"> <button class="btn" type="button">Select</button>  </h1></div> -->
+    <h1> Welcome to Evan's Draft Game</h1>
+    <h1 v-for="players in Info">  
+     {{players.Name}}
+    </h1>
+
 </template>
+
+
+
+
+
+<!--  <template>
+    <div class="Card"> <h1 class="text">{{ Name }}<img class="img" :src="Image"> <button class="btn" type="button">Select</button>  </h1></div>
+</template>
+<script setup>
+const props = defineProps({
+   Name: String,
+    Image : String,
+})
+</script>
+<style lang="scss" scoped>
+</style> --> 
